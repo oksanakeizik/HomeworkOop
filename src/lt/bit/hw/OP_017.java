468b3982bfd3813b07dc5fbd9e65ca615ae4e930
oksanakeizik <51049258+oksanakeizik@users.noreply.github.com>
2019-08-12 00:27:30 +0300

    Homework

--- src/lt/bit/hw/OP_017.java
+++ src/lt/bit/hw/OP_017.java
@@ -1,18 +1,24 @@
 package lt.bit.hw;
 
-/** LT Parašykite programą, kuri išveda į ekraną daugybos lentelę iki 10.
+/**
+ * LT Parašykite programą, kuri išveda į ekraną daugybos lentelę iki 10.
  * EN Write a program that outputs a multiplication table of up to 10 on the screen.
  */
 
 public class OP_017 {
     public static void main(String[] args) {
-        int start = 0;
-        for (int i = 1; i <= 10; i++) {
-            for (int k = 1; k <= 10; k++) {
-                start = i * k;
-                System.out.print(start + " ");
-            }
-            System.out.println();
+        int i, j;
+        System.out.printf(" * |");
+        for (i = 1; i < 10; i++)
+            System.out.printf("%2d ", i);
+        System.out.printf("\n---|------------");
+        System.out.printf("----------------\n");
+        for (i = 1; i < 10; i++) {
+            System.out.printf("%2d |", i);
+            for (j = 1; j < 10; j++)
+                System.out.printf("%2d ", i * j);
+            System.out.printf("\n");
         }
+
     }
 }
--- src/lt/bit/hw/OP_022.java
+++ src/lt/bit/hw/OP_022.java
@@ -1,5 +1,7 @@
 package lt.bit.hw;
 
+import java.util.Random;
+
 /**
  * LT Parašykite programą, kuri randa didžiausią String tipo masyvo elementą.
  * https://docs.oracle.com/javase/8/docs/api/java/util/Random.html
@@ -8,6 +10,26 @@ package lt.bit.hw;
 
 public class OP_022 {
     public static void main(String[] args) {
+        int a[] = new int[10];
+        Random random = new Random();
 
+        for (int i = 0; i < a.length; i++) {
+            a[i] = random.nextInt(9 - 0) + 0;
+            System.out.println(a[i]);
+        }
+
+        int max = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            a[i] = max;
+
+
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[j] > max) {
+                    max = a[j];
+                }
+            }
+        }
+        System.out.println("Largest element: " + max);
     }
 }
